039d909949d96cf933a60dc60e254a6cf2f4a28c
SeongUkJang <seonguk94@gmail.com>
2025-03-07 11:02:54 +0900

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('.userInput')
+const wordDisplay = document.querySelector('.currentWord')
+const btn = document.querySelector('.submitButton')
+const msg = document.querySelector('.message')
