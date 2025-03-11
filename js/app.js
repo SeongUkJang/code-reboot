c4111e43f8ff88ef0d2d4dfdb9b51d7fb5f49d88
SeongUkJang <seonguk94@gmail.com>
2025-03-11 16:18:13 +0900

    Add files via upload

--- js/app.js
+++
@@ -1,4 +0,0 @@
-const input = document.querySelector('.userInput')
-const wordDisplay = document.querySelector('.currentWord')
-const btn = document.querySelector('.submitButton')
-const msg = document.querySelector('.message')
--- js/script.js
+++ js/script.js
@@ -3,38 +3,53 @@ const wordDisplay = document.querySelector('.currentWord')
 const btn = document.querySelector('.submitButton')
 const msg = document.querySelector('.message')
 
-let word = '사과'
-wordDisplay.textContent = word
-msg.textContent = '끝말잇기 시작'
 
-function check(){
+let word = '사과'
+
+wordDisplay.textContent = word
+
+// 끝말잇기 시작!
+msg.textContent = '끝말잇기 시작!'
+
+
+function check() {
+
     const userWord = input.value.trim()
-    if(!userWord){
-        msg.textContent = '단어를 입력하세요'
+
+    if (!userWord) {
+        msg.textContent = '단어를 입력하세요!'
         return
     }
+
+    // const lastChar = word[word.length - 1]
+    const lastChar = word.at(-1)
+
+    // console.log(lastChar, userWord[0]);
+
+    if (lastChar !== userWord[0]) {
+        msg.textContent = `"${lastChar}"(으)로 시작되는 단어를 입력하세요`
+        input.value = ''
+    } else {
+
+        msg.textContent = ' 성공~ 다음 단어를 입력하세요'
+        word = userWord
+        wordDisplay.textContent = word
+        input.value = ''
+    }
+
+
 }
 
-//     const lastChar = word[word.length -1]
-//     console.log(lastChar,userWord[0]);
-//     if(lastChar !== userWord[0]){
-//         msg.textContent = `"${lastChar}"(으)로 시작되는 단어를 입력하세요` 
-//         input.value =''
-//     }
-//     else{
-//         msg.textContent = `성공! 다음 단어를 입력하세요`
-//         input.value =''
-//         word = userWord
-//         wordDisplay.textContent = word
-//     }
-// }
+input.addEventListener('keypress', (e) => {
 
-btn.addEventListener('click',()=>{
-    check()
+    if (e.key === 'Enter') {
+        check()
+    }
+
 })
 
-input.addEventListener('keypress', (e)=>{
-    if (e.key === 'Enter'
-    )
+
+btn.addEventListener('click', () => {
     check()
+
 })
